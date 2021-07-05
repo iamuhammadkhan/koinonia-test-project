57845aac641feb83b961e927071fe6e156f659f1
iamuhammadkhan <imuhammad.khan@icloud.com>
2021-07-05 23:14:19 +0500

    Initial Commit

---
+++ koinonia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  koinonia
+//
+//  Created by Muhammad Khan on 7/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
